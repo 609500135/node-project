baddc859cfc3ca9b0ada9e3a48fe84b5205b6572
Your Name <609500135@qq.co>
2019-01-23 14:47:38 +0800

    banner 增加

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-//引入的路由中间件的文件
+// 引入的路由中间的文件
 const indexRouter = require('./routes/index');
+const bannerRouter = require('./routes/banner');
 
-//设置静态文件托管
-app.use(express.static(path.resolve(__dirname,'./public')));
+// 设置 静态文件托管
+app.use(express.static(path.resolve(__dirname, './public')));
 
-//使用中间件--cookie
-app.use(cookieParser())
+// 使用中间件 - cookie
+app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-//设置模板文件的路径与使用的什么模板引擎
-app.set('views',path.resolve(__dirname,'./views'));
+// 设置模板文件的路径与使用的什么模板引擎
+app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+// 路由中间件的使用
+app.use('/', indexRouter);
+app.use('/banner', bannerRouter);
 
-//路由中间件的使用
-app.use('/',indexRouter);
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 //首页 http://localhost:3000/
-router.get('/',(req, res) =>{
-    res.render('index')
-})
-
+router.get('/',(req, res) => {
+    res.render('index');
+});
 
 //banner 页面
 router.get('/banner.html', (req, res) => {
